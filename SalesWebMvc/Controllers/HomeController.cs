9453fb3b9d86646f7e6a69c616c102e998a96350
João Santos <jvitorpereira10@gmail.com>
2023-02-06 19:40:04 -0300

    First controller and Razor pages tests.

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
     public IActionResult About()
     {
-      ViewData["Message"] = "Your application description page.";
+      ViewData["Message"] = "Sales Web MVC App from C#.";
+      ViewData["Developer"] = "João Vitor Santos.";
 
       return View();
     }
